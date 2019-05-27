a4cd00865e239e773e433719f622199e55ac22c0
Vishwaak <chandranvishwaak@gmail.com>
2019-05-27 08:21:25 +0530

    Automatic content downloader

--- weather_app/app.py
+++ weather_app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask ,render_template
-import logging
+import json
 from flask import request
+import subprocess
 
 app = Flask(__name__)
 
@@ -10,9 +11,10 @@ def index():
 
 @app.route('/' , methods=['POST'])
 def my_form():
-    text = request.form['text']
-    process_text = text.upper()
-    return render_template('data.html' , value = process_text)
+    reponce = request.form['text']
+    result = subprocess.check_output(reponce , shell=True)
+    result = result.split()
+    return render_template('data.html' , value = result)
 
 if __name__ =='__main__':
-    app.run(host = '0.0.0.0' , port=8080 , debug=True)
+    app.run(host = '0.0.0.0' , port=2000 , debug=True)
